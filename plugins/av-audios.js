64de18728d4f026ad8578aa66828610bc5948978
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-05-18 13:37:48 -0500

    Actualizar av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -19,5 +19,5 @@ let audioMsg = {
   'buenas tardes': 'https://b.top4top.io/m_2826v2zg51.mp3',
   'buenas noches': 'https://i.top4top.io/m_2826o8rfj1.mp3',
   'sad': 'https://h.top4top.io/m_2826mcim21.mp3',
-  '@5491168352204|@59172945992': 'https://l.top4top.io/m_2492i4mdu1.mp3'
+  '@593992402778|@593992402778': 'https://l.top4top.io/m_2492i4mdu1.mp3'
 }
