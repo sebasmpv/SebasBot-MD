7001a1a77f825c327a26d56b3e575c7fd75c9054
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-05-18 22:16:25 -0500

    Actualizar _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -31,7 +31,7 @@ export async function before(m, { conn }) {
     contextInfo: {
     	isForwarded: true,
     forwardedNewsletterMessageInfo: {
-      newsletterJid: id_canal,
+      newsletterJid: fgcanal,
       serverMessageId: 100,
       newsletterName: nam,
     }, 
