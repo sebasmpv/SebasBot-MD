5b4a5ff844c3e82e5c5f53d7ca13abb9e86b61a8
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-05-18 22:21:08 -0500

    Actualizar _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -11,8 +11,11 @@ let _muptime
         setTimeout(resolve, 1000)
       }) * 1000
     }
+*/
     let muptime = clockString(_muptime)
 		let bio = `\n🟢 Tiempo Activo ${muptime}\n\n┃ 💎 /support`
+
+*/
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
